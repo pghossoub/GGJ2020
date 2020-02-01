621cf0bd4e9cf14d338e13278b6259f1e19f4c44
Pierre Ghossoub <pghossoub@hotmail.com>
2020-02-02 00:40:01 +0100

    Dropzone glass & WIP DropzoneWorkbench

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public abstract class DropZone : MonoBehaviour
 {
-    public abstract void DropItem(GameObject item, Vector3 dropPosition);
-        
+    public MouseDragDrop m_mouseDragDrop;
+
+    public virtual void DropItem(GameObject item, Vector3 dropPosition)
+    {
+        // item.transform.position = dropPosition;
+        var rb = item.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        //rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        m_mouseDragDrop.DropItem(gameObject, collision.contacts[0].point);
+    }
+
 }

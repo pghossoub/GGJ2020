2ea653ceb9face094b51a84526bfe8aa7be1f60f
Pierre Ghossoub <pghossoub@hotmail.com>
2020-02-01 13:00:21 +0100

    Script MouseOutline

---
+++ Assets/Scripts/MouseOutline.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOutline : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    public Camera mainCamera;
+
+    private Outline outline;
+
+    void Awake()
+    {
+        //outline = GetComponent<Outline>();
+    }
+    void Update()
+    {
+        Vector3 mousePosition3D = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+
+        RaycastHit hit;
+
+        bool mouseOnObject = Physics.Raycast(
+            mainCamera.ScreenPointToRay(mousePosition3D),
+            out hit,
+            float.PositiveInfinity, 
+            layerMask
+            );
+
+
+        if (mouseOnObject)
+        {
+            GameObject objectOnMouse = hit.collider.gameObject;
+            outline = objectOnMouse.GetComponent<Outline>();
+            outline.OutlineWidth = 10f;
+            outline.OutlineColor = Color.yellow;
+            Debug.Log("mouseOnObject");
+        }
+        else
+            if(outline != null)
+                outline.OutlineWidth = 0f;
+    }
+}

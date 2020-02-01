bce4ec237e03986dfce875238ec43d03644b4595
dalexmuro <dalexmuro@gmail.com>
2020-02-01 14:29:45 +0100

    Gestion de Cameras: Start, Normal et Loupe

--- Assets/TestStateMachine.cs
+++ Assets/TestStateMachine.cs
@@ -13,6 +13,9 @@ public class TestStateMachine : MonoBehaviour
     {
         m_animator.SetBool(Animator.StringToHash("IsLoupe"), true);
         m_animator.SetBool(Animator.StringToHash("IsNormal"), false);
+        m_animator.SetBool(Animator.StringToHash("IsStart"), false);
+        m_animator.SetBool(Animator.StringToHash("IsSalle"), false);
+        m_animator.SetBool(Animator.StringToHash("IsMontage"), false);
     }
 
     [ContextMenu("State > Normal")]
@@ -20,6 +23,39 @@ public class TestStateMachine : MonoBehaviour
     {
         m_animator.SetBool(Animator.StringToHash("IsLoupe"), false);
         m_animator.SetBool(Animator.StringToHash("IsNormal"), true);
+        m_animator.SetBool(Animator.StringToHash("IsStart"), false);
+        m_animator.SetBool(Animator.StringToHash("IsSalle"), false);
+        m_animator.SetBool(Animator.StringToHash("IsMontage"), false);
+    }
+
+    [ContextMenu("State > Start")]
+    public void SwitchToStart()
+    {
+        m_animator.SetBool(Animator.StringToHash("IsLoupe"), false);
+        m_animator.SetBool(Animator.StringToHash("IsNormal"), false);
+        m_animator.SetBool(Animator.StringToHash("IsStart"), true);
+        m_animator.SetBool(Animator.StringToHash("IsSalle"), false);
+        m_animator.SetBool(Animator.StringToHash("IsMontage"), false);
+    }
+
+    [ContextMenu("State > Montage")]
+    public void SwitchToMontage()
+    {
+        m_animator.SetBool(Animator.StringToHash("IsLoupe"), false);
+        m_animator.SetBool(Animator.StringToHash("IsNormal"), false);
+        m_animator.SetBool(Animator.StringToHash("IsStart"), false);
+        m_animator.SetBool(Animator.StringToHash("IsSalle"), false);
+        m_animator.SetBool(Animator.StringToHash("IsMontage"), true);
+    }
+
+    [ContextMenu("State > Salle")]
+    public void SwitchToSalle()
+    {
+        m_animator.SetBool(Animator.StringToHash("IsLoupe"), false);
+        m_animator.SetBool(Animator.StringToHash("IsNormal"), false);
+        m_animator.SetBool(Animator.StringToHash("IsStart"), false);
+        m_animator.SetBool(Animator.StringToHash("IsSalle"), true);
+        m_animator.SetBool(Animator.StringToHash("IsMontage"), false);
     }
 
     private void Update()
@@ -34,6 +70,21 @@ public class TestStateMachine : MonoBehaviour
             case ScreenState.NORMAL:
                 {
                     SwitchToNormal();
+                    break;
+                }
+            case ScreenState.START:
+                {
+                    SwitchToStart();
+                    break;
+                }
+            case ScreenState.MONTAGE:
+                {
+                    SwitchToMontage();
+                    break;
+                }
+            case ScreenState.SALLE:
+                {
+                    SwitchToSalle();
                     break;
                 }
         }

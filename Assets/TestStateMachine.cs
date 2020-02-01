614a7851b3c5f334eaa1643c07af6b892064ff8c
dalexmuro <dalexmuro@gmail.com>
2020-02-01 16:19:35 +0100

    toto

--- Assets/TestStateMachine.cs
+++ Assets/TestStateMachine.cs
@@ -57,7 +57,6 @@ public class TestStateMachine : MonoBehaviour
         m_animator.SetBool(Animator.StringToHash("IsSalle"), true);
         m_animator.SetBool(Animator.StringToHash("IsMontage"), false);
     }
-<<<<<<< HEAD
 
     private void Update()
     {
@@ -90,6 +89,4 @@ public class TestStateMachine : MonoBehaviour
                 }
         }
     }
-=======
->>>>>>> 7c59bc9aa7d4edd767f42350f980c6344a1395f2
 }

24f9f1da0c4b8cc0e3bc10cc7d84cfb2d741029b
Matthieu Golsenne <marsheleene@gmail.com>
2020-02-01 13:10:57 +0100

    Oubli du script Timer.cs

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using Marsheleene.Variables;
+
+public class Timer : MonoBehaviour
+{
+    public FloatReference m_startingTime;
+    public FloatReference m_currentTime;
+
+    private void Start()
+    {
+        m_currentTime.Value = m_startingTime.Value;
+    }
+
+    private void Update()
+    {
+        m_currentTime.Value -= Time.deltaTime;
+    }
+}

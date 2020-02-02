5207fcdcf7c2aeb89acb4c0383dd421d552d8ed7
Matthieu Golsenne <marsheleene@gmail.com>
2020-02-02 15:08:10 +0100

    Workbench

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
     public string m_currentLevel;
     public string m_nextLevel;
+
+    public VideoFragmentList m_workbenchFragments;
+
+    private void Awake()
+    {
+        m_workbenchFragments.m_list = new List<VideoFragment>(6);
+    }
+
 
     [ContextMenu("NextLevel")]
     public void LoadNextLevel()
